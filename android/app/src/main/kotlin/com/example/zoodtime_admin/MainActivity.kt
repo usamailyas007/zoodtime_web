01408c7c198086c2a2bc466eb6420665417f010a
usamailyas007 <usil7d@gmail.com>
2025-07-13 14:10:12 +0500

    gh-pages branch created

--- android/app/src/main/kotlin/com/example/zoodtime_admin/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.zoodtime_admin
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/screens/sidemenu/controller/sidemenu_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-
-class SideMenuController extends GetxController {
-  var selectedIndex = 0.obs;
-
-  void onItemTapped(int index) {
-    selectedIndex.value = index;
-  }
-}
